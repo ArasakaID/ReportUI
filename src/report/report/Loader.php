d32ff16cc878f5db514f84b1cadbb3be51cdc1a0
MasApip <52627239+MasApip@users.noreply.github.com>
2019-10-27 09:28:02 +0700

    Update Loader.php

--- src/report/report/Loader.php
+++ src/report/report/Loader.php
@@ -51,6 +51,58 @@ class Loader extends PluginBase implements Listener {
 					if ($data["view"] === false) {
 						$reports++;
 						break;
+<?php
+
+namespace report\report;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use report\gui\GuiReport;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\{CommandSender, Command};
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
+
+class Loader extends PluginBase implements Listener {
+
+	/** @var Loader */
+	public static $plugin;
+
+	/**
+	* @return void
+	*/
+	public function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		self::$plugin = $this;
+	}
+
+	/**
+	* @return Loader
+	*/
+	public static function getInstance() {
+		return self::$plugin;
+	}
+
+	/**
+	* @param Player|Null $player
+	*/
+	public function checkReports(?Player $player = null) {
+		$config = new Config($this->getDataFolder().'reports.yml', Config::YAML);
+		$config = $config->getAll();
+		if (empty($config)) {
+			return false;
+		}
+		$reports = 0;
+		foreach ($config as $user => $datas) {
+			foreach ($datas as $data) {
+				if (isset($data["view"])) {
+					if ($data["view"] === false) {
+						$reports++;
+						break;
 					}
 				}
 			}
@@ -107,5 +159,4 @@ class Loader extends PluginBase implements Listener {
 		return true;
 	}
 
-	private function checkUpdates(){$this->getLogger()->info(TextFormat::DARK_PURPLE.base64_decode('Q29tcHJvYmFuZG8gc2kgaGF5IHVuYSBudWV2YSB2ZXJzaW9uIGRpc3BvbmlibGUuLi4='));try{$tkgq_8d777f385d3d=file_get_contents(base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0RhcmtCeXgvUmVwb3J0TWFuYWdlci9tYXN0ZXIvdXBkYXRlLmpzb24='));}catch(\ErrorException $wqly_3cf804e7182a){$this->getLogger()->notice(TextFormat::DARK_PURPLE.base64_decode('RXJyb3IgYWwgY29tcHJvYmFyIGFjdHVhbGl6YWNpb25lcyE='));return false;}if($tkgq_8d777f385d3d===null)return;$tkgq_8d777f385d3d=json_decode($tkgq_8d777f385d3d);$jfxe_02bd92faa38a=$tkgq_8d777f385d3d->author;$kurp_2af72f100c35=$tkgq_8d777f385d3d->version;$tkum_5d9449e8d850=$this->getDescription()->getAuthors();$zwia_c8ff68fb014c=$this->getDescription()->getVersion();if(!in_array($jfxe_02bd92faa38a,$tkum_5d9449e8d850)){$uzjd_0b23512128b7=$this->getDescription()->getName();$this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin($uzjd_0b23512128b7));return;}if($kurp_2af72f100c35>$zwia_c8ff68fb014c){$this->getLogger()->info(TextFormat::DARK_PURPLE.base64_decode('QWN0dWFsaXphY2lvbiBkaXNwb25pYmxlIQ=='));}else{$this->getLogger()->info(TextFormat::DARK_PURPLE.base64_decode('VGllbmVzIGxhIHVsdGltYSB2ZXJzaW9uIQ=='));return;}}
 }
